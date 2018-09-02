44fb20c13806fdcb9fcbf472a27fe83706996484
Sebastian <sbarcenas00@gmail.com>
2018-09-01 23:20:58 -0500

    Estructura del proyecto

---
+++ src/com/estructura/colaimpresion/model/ColaImpresion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.estructura.colaimpresion.model;
+
+/**
+ *
+ * @author Sjbárcenas
+ */
+public class ColaImpresion {
+    
+}
